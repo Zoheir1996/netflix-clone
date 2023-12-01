1d4dd3f3014be87ce884f9595024a9b2ca3758ba
Zoheir1996 <75224982+Zoheir1996@users.noreply.github.com>
2023-12-01 02:57:17 +0100

    Initnialisation du projet

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import Magnify from "vue-material-design-icons/Magnify.vue";
+import HomeOutline from "vue-material-design-icons/HomeOutline.vue";
+import TrendingUp from "vue-material-design-icons/TrendingUp.vue";
+import Television from "vue-material-design-icons/Television.vue";
+import MovieOutline from "vue-material-design-icons/MovieOutline.vue";
+import Plus from "vue-material-design-icons/Plus.vue";
+import ChevronLeft from "vue-material-design-icons/ChevronLeft.vue";
+
+import { useMovieStore } from "./stores/useMoveStore";
+import { storeToRefs } from "pinia";
+const useMovie = useMovieStore();
+const { movies, showFullVideo } = storeToRefs(useMovie);
+</script>
+
+<template>
+  <div class="text-red-400">TEXT HERE</div>
+</template>
