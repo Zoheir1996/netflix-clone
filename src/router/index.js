857e0eddee1015e67375cf30aa2d147b5b426c52
Zoheir1996 <75224982+Zoheir1996@users.noreply.github.com>
2023-12-09 03:17:37 +0100

    Creation des routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Homeview from "../views/Homeview.vue";
+import Searchview from "../views/Searchview.vue";
+
+const routes = [
+  { path: "/", component: Homeview },
+  { path: "/search", component: Searchview },
+  // Ajoutez d'autres routes selon vos besoins
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-  ],
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
